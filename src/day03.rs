8570befe53a80a8fd9d99146e164a3b6650f705b
Shitesh Arihunta <arihunta@gmail.com>
2022-12-04 14:07:33 +0200

    Cleanup day 03-01, implement day 03-02

--- src/day03.rs
+++ src/day03.rs
@@ -2,63 +2,53 @@ use std::{io::{Lines, BufReader}, fs::File, collections::HashSet};
 
 pub fn day03_01(lines: Lines<BufReader<File>>) -> i32 {
 
-    let mut priority_sum : i32 = 0;
+    return lines
+        .map( |line| line.unwrap() )
+        .map( |rucksack| -> i32 {
+            let compartment1 = &rucksack[..(rucksack.len() / 2)];
+            let compartment2 = &rucksack[(rucksack.len() / 2)..];
 
-    for rucksack in lines {
-        let rucksack = rucksack.unwrap();
-        let compartment1 = &rucksack[..(rucksack.len() / 2)];
-        let compartment2 = &rucksack[(rucksack.len() / 2)..];
+            let compartment1 : HashSet<char> = HashSet::from_iter(compartment1.chars());
 
-        let mut x = HashSet::new();
-
-        for chr in compartment1.bytes() {
-            x.insert(chr);
-        }
-
-        for chr in compartment2.bytes() {
-            if x.contains(&chr) {
-                priority_sum += to_priority(chr);
-                break;
+            match compartment2.chars().find( |chr| compartment1.contains(chr) ) {
+                Some(chr) => to_priority(chr as i32),
+                None => 0,
             }
+        } )
+        .sum();
+
+}
+
+pub fn day03_02(mut lines: Lines<BufReader<File>>) -> i32 {
+
+    let mut sum = 0;
+
+    loop {
+        let r1 = lines.next();
+        if r1.is_none() {
+            break;
         }
+        let r1 = r1.expect("No more lines for r1").expect("Error reading line for r1");
+        let r2 = lines.next().expect("No more lines for r2").expect("Error reading line for r2");
+        let r3 = lines.next().expect("No more lines for r3").expect("Error reading line for r3");
+
+        let mut r1 : HashSet<char> = HashSet::from_iter(r1.chars());
+        let r2 : HashSet<char> = HashSet::from_iter(r2.chars());
+        let r3 : HashSet<char> = HashSet::from_iter(r3.chars());
+
+        r1.retain( |chr| r2.contains(chr) );
+        r1.retain( |chr| r3.contains(chr) );
+
+        let x = r1.iter().next().expect("No common chars found");
+        sum += to_priority(*x as i32);
 
     }
-
-    return priority_sum;
-
-}
-
-pub fn day03_02(lines: Lines<BufReader<File>>) -> i32 {
-
-    let mut priority_sum : i32 = 0;
-
-    // for rucksack in lines {
-    //     let rucksack = rucksack.unwrap();
-    //     let compartment1 = &rucksack[..(rucksack.len() / 2)];
-    //     let compartment2 = &rucksack[(rucksack.len() / 2)..];
-
-    //     let mut x = HashSet::new();
-
-    //     x.
-
-    //     for chr in compartment1.bytes() {
-    //         x.insert(chr);
-    //     }
-
-    //     for chr in compartment2.bytes() {
-    //         if x.contains(&chr) {
-    //             priority_sum += to_priority(chr);
-    //             break;
-    //         }
-    //     }
-
-    // }
-
-    return priority_sum;
+    
+    return sum;
 
 }
 
-fn to_priority(item: u8) -> i32 {
+fn to_priority(item: i32) -> i32 {
     if item < 91 {
         i32::from(item) - 38
     } else {
--- src/tests/day03_tests.rs
+++ src/tests/day03_tests.rs
@@ -30,30 +30,30 @@ fn test_day03_01() {
 
 }
 
-// #[test]
-// fn test_day03_02_01() {
+#[test]
+fn test_day03_02_01() {
 
-//     let f = match File::open("data/03_01") {
-//         Ok(f) => f,
-//         Err(_) => panic!("Could not oepn file"),
-//     };
+    let f = match File::open("data/03_01") {
+        Ok(f) => f,
+        Err(_) => panic!("Could not oepn file"),
+    };
 
-//     let result = day03::day03_02(BufReader::new(f).lines());
+    let result = day03::day03_02(BufReader::new(f).lines());
 
-//     assert_eq!(12, result);
+    assert_eq!(70, result);
 
-// }
+}
 
-// #[test]
-// fn test_day03_02() {
+#[test]
+fn test_day03_02() {
 
-//     let f = match File::open("data/03") {
-//         Ok(f) => f,
-//         Err(_) => panic!("Could not oepn file"),
-//     };
+    let f = match File::open("data/03") {
+        Ok(f) => f,
+        Err(_) => panic!("Could not oepn file"),
+    };
 
-//     let result = day03::day03_02(BufReader::new(f).lines());
+    let result = day03::day03_02(BufReader::new(f).lines());
 
-//     assert_eq!(15_457, result);
+    assert_eq!(2_973, result);
 
-// }
+}
